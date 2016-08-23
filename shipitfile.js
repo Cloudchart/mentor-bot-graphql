19eb5b200569c93b03d1c5dce13667ce7679fce0
Eugene Kovalev <seanchas@gmail.com>
2016-08-23 18:42:26 +0300

    Added shipit-shared module

--- shipitfile.js
+++ shipitfile.js
@@ -1,5 +1,6 @@
 module.exports = function(shipit) {
   require('shipit-deploy')(shipit)
+  require('shipit-shared')(shipit)
   require('shipit-npm')(shipit)
 
   shipit.initConfig({
@@ -9,11 +10,25 @@ module.exports = function(shipit) {
       ignores       : ['.git', 'node_modules'],
       repositoryUrl : 'git@github.com:Cloudchart/mentor-bot-graphql.git',
       shallowClone  : true,
-    },
+
+      shared        : {
+        overwrite   : true,
+        files       : ['.env'],
+      }
+  },
 
     staging: {
       servers: 'app@mentor-staging.cochart.net'
     }
+  })
+
+
+  shipit.blTask('foreman:start', function() {
+    return shipit.remote(`NODE_ENV=production /home/app/mentor-bot-graphql/shared/start`)
+  })
+
+  shipit.on('cleaned', function() {
+    return shipit.start('foreman:start')
   })
 
 }
