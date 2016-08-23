66f1d6efb9b4558b8465358cf57287ba6f370893
Eugene Kovalev <seanchas@gmail.com>
2016-08-23 15:27:37 +0300

    Modified shirt configuration

--- shipitfile.js
+++ shipitfile.js
@@ -1,19 +1,19 @@
 module.exports = function(shipit) {
   require('shipit-deploy')(shipit)
+  require('shipit-npm')(shipit)
 
   shipit.initConfig({
     default: {
       workspace     : '/tmp/mentor-graphql/workspace',
       deployTo      : '/home/app/mentor-bot-graphql',
       ignores       : ['.git', 'node_modules'],
+      repositoryUrl : 'git@github.com:Cloudchart/mentor-bot-graphql.git',
       shallowClone  : true,
     },
 
     staging: {
-      servers: 'mentor-staging.cochart.net'
+      servers: 'app@mentor-staging.cochart.net'
     }
   })
-
-
 
 }
